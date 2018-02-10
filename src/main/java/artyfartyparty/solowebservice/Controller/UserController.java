c058e2caa05cc1f041784bf4d6ec05d14f84d376
sigurlaug <sigurlaugthordar@gmail.com>
2018-02-10 15:52:39 +0000

    smá laga

--- src/main/java/artyfartyparty/solowebservice/Controller/UserController.java
+++ src/main/java/artyfartyparty/solowebservice/Controller/UserController.java
@@ -3,6 +3,7 @@ package artyfartyparty.solowebservice.Controller;
 import artyfartyparty.solowebservice.Model.Request.AddUserRequest;
 import artyfartyparty.solowebservice.Model.User;
 import artyfartyparty.solowebservice.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,19 +12,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
     private UserRepository userRepository;
 
+    @Autowired
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value="/all", method = RequestMethod.GET)
     public List<User> findAllUsers() {
         return userRepository.findAll();
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @RequestMapping(value="/add", method = RequestMethod.POST)
     public void AddUser(@RequestBody AddUserRequest addUserRequest) {
         User user = new User();
         user.setName(addUserRequest.getName());
