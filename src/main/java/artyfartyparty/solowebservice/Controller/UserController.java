43fc0c7c655674accd880b03eceb232c5ff6876b
sigurlaug <sigurlaugthordar@gmail.com>
2018-02-13 21:44:28 +0000

    plís !

--- src/main/java/artyfartyparty/solowebservice/Controller/UserController.java
+++ src/main/java/artyfartyparty/solowebservice/Controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -21,6 +22,7 @@ public class UserController {
     }
 
     @RequestMapping(value="/all", method = RequestMethod.GET)
+    @ResponseBody
     public List<User> findAllUsers() {
         List<User> users = userRepository.findAll();
         return users;
