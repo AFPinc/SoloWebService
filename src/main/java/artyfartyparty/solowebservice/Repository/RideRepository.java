3fd9237287b6f4ca998b8af34853952b9da65b32
sigurlaug <sigurlaugthordar@gmail.com>
2018-02-21 13:29:21 +0000

    Add and get all rides

---
+++ src/main/java/artyfartyparty/solowebservice/Repository/RideRepository.java
@@ -0,0 +1,7 @@
+package artyfartyparty.solowebservice.Repository;
+
+import artyfartyparty.solowebservice.Model.Ride;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RideRepository extends JpaRepository<Ride, Long> {
+}
