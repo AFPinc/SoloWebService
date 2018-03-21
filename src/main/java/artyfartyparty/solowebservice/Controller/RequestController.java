c306554f83ca46a2738c007fbae6b3959e8a92d0
sigurlaug <sigurlaugthordar@gmail.com>
2018-03-21 15:40:03 +0000

    all requests

--- src/main/java/artyfartyparty/solowebservice/Controller/RequestController.java
+++ src/main/java/artyfartyparty/solowebservice/Controller/RequestController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -32,7 +33,8 @@ public class RequestController {
         return new ResponseEntity<>(request, HttpStatus.OK);
     }
 
-    @RequestMapping(value="/all", method = RequestMethod.POST)
+    @RequestMapping(value="/all", method = RequestMethod.GET)
+    @ResponseBody
     public List<Request> AllRequests() {
 
         return requestRepository.findAll();
